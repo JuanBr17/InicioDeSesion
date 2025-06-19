913b23ddfb28dfe0d87cd80d5bb750ecc7992910
JuanBr17 <juansp.br17@gmail.com>
2025-06-18 22:45:59 -0500

    Add files via upload

---
+++ InicioDeSesion-main/index.js
@@ -0,0 +1,15 @@
+var usuario= "a@gmail.com"
+var contrasena="1234"
+
+var userg= document.getElementById("Entrada1").value
+var pass= document.getElementById("Entrada2").value  
+
+function enviar(Entrada1, Entrada2) {
+    if (Entrada1 === userg){
+        if (Entrada2 === pass) {
+            alert("Bienvenido!");
+        }
+    } else {
+        alert("No se pudo iniciar sesión");
+    }
+}
